881a9b1105ac97de87b0424b9a804879fb94a023
tomassklenicka <56340124+tomassklenicka@users.noreply.github.com>
2020-10-04 14:46:24 +0200

    Add files via upload

---
+++ servlet/muj-web/src/main/java/MyServlet.java
@@ -0,0 +1,36 @@
+
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import javax.servlet.ServletException;
+
+@WebServlet("/timezone")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html;charset=utf-8");
+        PrintWriter out = res.getWriter();
+        String input = req.getParameter("zone");
+        Set<String> zoneIds = ZoneId.getAvailableZoneIds();
+        boolean isValid = false;
+        for (String str : zoneIds) {
+            if (str != null && str.equals(input)) {
+                isValid = true;
+                break;
+            }
+        }
+        String info = "Wrong time zone";
+        if (isValid) {
+            ZoneId zone = ZoneId.of(input);
+            ZonedDateTime now = ZonedDateTime.now(zone);
+            time = "Time: " + now;
+        }
+        out.println("<html><body>");
+        out.println(time);
+        out.println("</body></html>");
+    }
+}
